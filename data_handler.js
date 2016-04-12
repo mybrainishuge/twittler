3d8e5d8eddbe3c83ae9f9f2b57b0725a8f37f982
Richard May <mybrainishuge@users.noreply.github.com>
2016-04-11 21:40:31 -0700

    Reverted timestamps
    Realized my timestamps weren't displaying how I thought using moment.js
    so I reverted them back to native JS.  Will need to develop an interval
    that updates the timestamps periodically.

--- data_handler.js
+++ data_handler.js
@@ -35,8 +35,8 @@ $(document).ready(function() {
     var $time = $('<div class="tweet-head tweet-time"></div>');
     var $tweet = $('<div class="tweet group"></div>');
     $user.text('@' + tweet.user);
-    // $time.text(tweet.created_at.toLocaleString());
-    $time.text(moment().to(tweet.created_at));
+    $time.text(tweet.created_at.toLocaleString());
+    // $time.text(moment().to(tweet.created_at));
     $tweet.text(tweet.message);
     $tweet.prependTo($timeline);
     $time.prependTo($timeline);
@@ -51,8 +51,8 @@ $(document).ready(function() {
     var $time = $('<div class="tweet-head tweet-time"></div>');
     var $tweet = $('<div class="tweet group"></div>');
     $user.text('@' + tweet.user);
-    // $time.text(tweet.created_at.toLocaleString());
-    $time.text(moment().to(tweet.created_at));
+    $time.text(tweet.created_at.toLocaleString());
+    // $time.text(moment().to(tweet.created_at));
     $tweet.text(tweet.message);
     $user.appendTo($timeline);
     $user.click(viewUserTweets); // adds click behavior to username of tweet
@@ -184,8 +184,8 @@ var viewUserTweets = function() {
       var tweet = streams.users[user][index];
       var $time = $('<div class="tweet-head tweet-time"></div>');
       var $tweet = $('<div class="tweet group"></div>');
-      // $time.text(tweet.created_at.toLocaleString());
-      $time.text(moment().to(tweet.created_at));
+      $time.text(tweet.created_at.toLocaleString());
+      // $time.text(moment().to(tweet.created_at));
       $tweet.text(tweet.message);
       $time.appendTo($timeline);
       $tweet.appendTo($timeline);
@@ -201,4 +201,3 @@ var clearTwitBox = function() {
     this.value = '';
   }
 };
-
